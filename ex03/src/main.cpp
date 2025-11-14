30ec72b3e797e104de4a38dceb3ea12fcb8aed97
CRaph0705 <raphael.cochrane94@gmail.com>
2025-11-14 19:18:12 +0100

    ex03 draft

--- ex03/src/main.cpp
+++ ex03/src/main.cpp
@@ -6,13 +6,28 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 12:40:46 by rcochran          #+#    #+#             */
-/*   Updated: 2025/10/31 12:41:07 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/11/14 19:16:54 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 
-int	main (void)
+int main()
 {
-	return (0);
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+	ICharacter* me = new Character("me");
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	ICharacter* bob = new Character("bob");
+	me->use(0, *bob);
+	me->use(1, *bob);
+	delete bob;
+	delete me;
+	delete src;
+	return 0;
 }
